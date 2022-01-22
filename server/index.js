42da347cbdcd2d71d9762c0fa19029512d1ee4e5
Something <passagon.forwork@gmail.com>
2022-01-22 17:53:59 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "employee"
+})
+
+app.get('/newemployee', (req, res)=>{
+    db.query("SELECT * FROM newemployee" , (err, result)=>{
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/create', (req,res)=>{
+    const name = req.body.name
+    const age = req.body.age
+    const country = req.body.country
+    const position = req.body.position
+    const wage = req.body.wage
+
+    db.query("INSERT INTO newemployee (name, age, country, position, wage) VALUES(?,?,?,?,?)" ,[name, age, country, position, wage],(err,result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send("Values inserted");
+        }
+    });
+})
+
+app.put('/update', (req,res)=>{
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query("UPDATE newemployee SET wage = ? WHERE id = ?", [wage, id], (err,result)=>{
+        if(err){
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    })
+})
+
+app.delete('/delete/:id', (req,res)=>{
+    const id = req.params.id;
+    db.query("DELETE FROM newemployee WHERE id = ?", id, (err,result) =>{
+        if(err){
+            console.log(err);
+        }else {
+            res.send(result);
+        }
+    })
+})
+
+app.listen('3001', ()=>{
+    console.log('Server is running on port 3001');
+})
